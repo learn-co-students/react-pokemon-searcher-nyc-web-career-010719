9aa8a279757eda10431b89e2524155d120e3b587
github username <github email address>
2019-03-19 10:16:14 -0400

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,50 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      pic: "front"
+    }
+  }
+
+  currentPic = () => {
+    if (this.state.pic === "front"){
+      return this.props.pokemon.sprites.front
+    }
+    else {
+      return this.props.pokemon.sprites.back
+    }
+  }
+
+  findHp = () => {
+    let myStat =  this.props.pokemon.stats.find(stat => stat.name === "hp")
+    return myStat.value
+  }
+
+  handle = () => {
+    if (this.state.pic == "front"){
+      this.setState({pic: "back"})
+    }
+    else {
+      this.setState({pic: "front"})
+    }
+  }
+
   render() {
     return (
       <Card>
         <div>
-          <div className="image">
-            <img alt="oh no!" />
+          <div className="image" onClick={this.handle}>
+            <img src={this.currentPic()}alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {this.findHp()}
             </span>
           </div>
         </div>
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -5,9 +5,13 @@ import { Card } from 'semantic-ui-react'
 class PokemonCollection extends React.Component {
   render() {
     return (
+      <div>
+
       <Card.Group itemsPerRow={6}>
+      {this.props.pokemon.map(pokemon => <PokemonCard pokemon={pokemon}/>)}
         <h1>Hello From Pokemon Collection</h1>
       </Card.Group>
+      </div>
     )
   }
 }
