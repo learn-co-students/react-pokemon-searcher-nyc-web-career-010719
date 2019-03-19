a1f63a28099b29958b7cc9bd4099856485b35852
purplesands <mullenjoe13@gmail.com>
2019-03-19 15:58:17 -0400

    Done.

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,16 +5,77 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+
+  constructor() {
+    super()
+    this.state = ({
+      pokemon: [],
+      value: '',
+      isLoading:false,
+      results: [],
+    })
+  }
+
+  fetchPokemon(){
+    fetch('http://localhost:3000/pokemon')
+    .then(r => r.json())
+    .then(pokemon => this.setState({ pokemon:pokemon,results:pokemon }))
+
+  }
+
+  componentDidMount(){
+    this.setState({})
+    this.fetchPokemon()
+  }
+
+  componentWillMount() {
+    this.resetComponent()
+  }
+
+  resetComponent = () => {
+    this.setState({ isLoading: false, results: this.state.pokemon, value: '' })
+    this.fetchPokemon()
+}
+
+  handleResultSelect = (e, { result }) => {
+    this.setState({ value: result })
+    console.log(result)
+  }
+
+  handleSearchChange = (e, { value }) => {
+    this.setState({ isLoading: true, value })
+
+    setTimeout(() => {
+      if (this.state.value.length < 1) return this.resetComponent()
+
+      const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+      const isMatch = result => re.test(result.name)
+
+      this.setState({
+        isLoading: false,
+        results: _.filter(this.state.pokemon, isMatch)
+      })
+    }, 2)
+  }
+
   render() {
+    const { isLoading, value, results } = this.state
+    console.log(this.state.results)
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search
+            loading={isLoading}
+            onResultSelect={this.handleResultSelect}
+            onSearchChange={_.debounce(this.handleSearchChange, 0, { leading: true })}
+            results={results}
+            value={value}
+             />
         <br />
-        <PokemonCollection />
+        <PokemonCollection allPokemon={this.state.results} />
         <br />
-        <PokemonForm />
+        <PokemonForm reset={(e)=>this.componentDidMount(e)}/>
       </div>
     )
   }
