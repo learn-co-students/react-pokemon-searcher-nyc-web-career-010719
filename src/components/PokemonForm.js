e535d76d609c5abc7a2f263fafba06e004682e58
Your Name <github email address>
2019-03-18 20:48:03 -0400

    Done.

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -8,9 +8,48 @@ class PokemonForm extends React.Component {
     this.state = {
       name: '',
       hp: '',
-      frontUrl: '',
-      backUrl: ''
+      front: '',
+      back: ''
     }
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    }, () => console.log(this.state))
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const { name, hp, front, back } = this.state
+    console.log(front)
+    fetch('http://localhost:3000/pokemon', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        stats: [
+          {
+            value: hp,
+            name: 'hp'
+          }
+        ],
+        sprites: {
+          front: front,
+          back: back
+        }
+      })
+    })
+    .then(res => res.json())
+    .then(pokemon => this.props.addPokemon(pokemon))
+    .then(this.setState({
+      name: '',
+      hp: '',
+      front: '',
+      back: ''
+    }))
   }
 
   render() {
@@ -19,10 +58,33 @@ class PokemonForm extends React.Component {
         <h3>Add a Pokemon!</h3>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input
+              fluid label="Name"
+              placeholder="Name"
+              name="name"
+              onChange={this.handleChange}
+              value={this.state.name}
+            />
+            <Form.Input
+              fluid label="hp"
+              placeholder="hp" name="hp"
+              onChange={this.handleChange}
+              value={this.state.hp}
+            />
+            <Form.Input
+              fluid label="Front Image URL"
+              placeholder="url"
+              name="front"
+              onChange={this.handleChange}
+              value={this.state.front}
+            />
+            <Form.Input
+              fluid label="Back Image URL"
+              placeholder="url"
+              name="back"
+              onChange={this.handleChange}
+              value={this.state.back}
+            />
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,16 +5,44 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+  state = {
+    pokemon: [],
+    searchTerm: ''
+  }
+
+  handleSearchChange = (e, {value}) => {
+    this.setState({ searchTerm: value })
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/pokemon')
+    .then(res => res.json())
+    .then(pokemon => this.setState({ pokemon }))
+  }
+
+  addPokemon = newPokemon => {
+    this.setState({ pokemon: [...this.state.pokemon, newPokemon] })
+  }
+
   render() {
+    const searchedPokemon = this.state.pokemon.filter(pokemon =>
+      pokemon.name.includes(this.state.searchTerm)
+    )
+
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+       <Search onSearchChange={_.debounce(this.handleSearchChange, 500)} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection
+        pokemon={searchedPokemon}
+        />
         <br />
-        <PokemonForm />
+        <PokemonForm
+          addPokemon={this.addPokemon}
+          pokemon={this.state.pokemon}
+        />
       </div>
     )
   }
