2d67316e12ea0c917cb46a5012d86447b9eb293e
cannoli9917 <katie.pennachio@gmail.com>
2019-03-19 13:37:21 -0400

    Done.

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -13,16 +13,48 @@ class PokemonForm extends React.Component {
     }
   }
 
+  handleOnChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.target.reset()
+    let data = {
+      "name": this.state.name,
+      "stats": [{"value": this.state.hp, "name": "hp"}],
+      "sprites": {
+        "front": this.state.frontUrl,
+        "back": this.state.backUrl
+      }
+    }
+
+    fetch('http://localhost:3000/pokemon', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(resp => resp.json())
+    .then(pokemon => {
+      this.props.addPokemon(pokemon)
+    })
+
+  }
+
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input fluid label="Name" placeholder="Name" name="name" onChange={this.handleOnChange}/>
+            <Form.Input fluid label="hp" placeholder="hp" name="hp" onChange={this.handleOnChange}/>
+            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" onChange={this.handleOnChange}/>
+            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" onChange={this.handleOnChange}/>
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
