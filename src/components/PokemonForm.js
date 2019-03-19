23b4b1e2d6af8026012c7e4129df6a692f17dfc0
github username <github email address>
2019-03-19 18:50:38 -0400

    Done.

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -8,9 +8,52 @@ class PokemonForm extends React.Component {
     this.state = {
       name: '',
       hp: '',
-      frontUrl: '',
-      backUrl: ''
+      front: '',
+      back: ''
     }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = () => {
+    let data = {
+          "name": this.state.name,
+          "stats": [
+            {
+              "value": this.state.hp,
+              "name": "hp"
+            }
+          ],
+          "sprites": {
+            "front": this.state.front,
+            "back": this.state.back
+          }
+        }
+
+    fetch("http://localhost:3000/pokemon", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accepts": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then( json => {
+
+      this.setState({
+        name: '',
+        hp: '',
+        front: '',
+        back: ''
+      })
+
+      this.props.createPokemon(json)
+    })
   }
 
   render() {
@@ -19,10 +62,10 @@ class PokemonForm extends React.Component {
         <h3>Add a Pokemon!</h3>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input fluid onChange={ (e) => this.handleChange(e) } label="Name" placeholder="Name" name="name" value={this.state.name}/>
+            <Form.Input fluid onChange={ (e) => this.handleChange(e) } label="hp" placeholder="hp" name="hp" value={this.state.hp}/>
+            <Form.Input fluid onChange={ (e) => this.handleChange(e) } label="Front Image URL" placeholder="url" name="front" value={this.state.front}/>
+            <Form.Input fluid onChange={ (e) => this.handleChange(e) } label="Back Image URL" placeholder="url" name="back" value={this.state.back}/>
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -1,20 +1,126 @@
 import React from 'react'
 import PokemonCollection from './PokemonCollection'
 import PokemonForm from './PokemonForm'
+import SortForm from './SortForm'
+
 import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+  state = {
+    pokemon: [], // all pokemon
+    filteredPokemon: []
+    // search: ""
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/pokemon")
+    .then(res => res.json())
+    .then( json => {
+      // console.log(json)
+      this.setState({
+        pokemon: json,
+        filteredPokemon: json
+      })
+    })
+  }
+
+  handleSearch = (e) => {
+    let newPokemon = this.state.pokemon.filter( p => {
+      return p.name.includes(e.target.value.toLowerCase())
+    })
+
+    this.setState({ filteredPokemon: newPokemon })
+  }
+
+  createPokemon = (pokemon) => {
+    // console.log("gonna create a pokemon!", pokemon);
+    let newPokemon = [...this.state.pokemon, pokemon]
+    this.setState({
+      pokemon: newPokemon,
+      filteredPokemon: newPokemon
+    })
+  }
+
+  handleSort = (e) => {
+    // console.log("handle sort", e.target.value);
+    if (e.target.value === "name1") {
+      this.setState( prevState => {
+        return { filteredPokemon: prevState.filteredPokemon.sort(this.sortName1)}
+      })
+      // console.log(sorted);
+    } else if (e.target.value === "name2") {
+      this.setState( prevState => {
+        return { filteredPokemon: prevState.filteredPokemon.sort(this.sortName2)}
+      })
+    } else if (e.target.value === "default") {
+      this.setState( prevState => {
+        return { filteredPokemon: prevState.filteredPokemon.sort(this.sortDefault)}
+      })
+    } else if (e.target.value === "hp1") {
+      this.setState( prevState => {
+        return { filteredPokemon: prevState.filteredPokemon.sort(this.sortHp1)}
+      })
+    } else if (e.target.value === "hp2") {
+      this.setState( prevState => {
+        return { filteredPokemon: prevState.filteredPokemon.sort(this.sortHp2)}
+      })
+    }
+  }
+
+  sortName1(a,b) {
+    if (a.name < b.name)
+      return -1;
+    if (a.name > b.name)
+      return 1;
+    return 0;
+  }
+
+  sortHp1(a,b) {
+    if (a.stats.find( obj => obj.name === "hp" ).value < b.stats.find( obj => obj.name === "hp" ).value)
+      return -1;
+    if (a.stats.find( obj => obj.name === "hp" ).value > b.stats.find( obj => obj.name === "hp" ).value)
+      return 1;
+    return 0;
+  }
+
+  sortName2(a,b) {
+    if (a.name < b.name)
+      return 1;
+    if (a.name > b.name)
+      return -1;
+    return 0;
+  }
+
+  sortHp2(a,b) {
+    if (a.stats.find( obj => obj.name === "hp" ).value < b.stats.find( obj => obj.name === "hp" ).value)
+      return 1;
+    if (a.stats.find( obj => obj.name === "hp" ).value > b.stats.find( obj => obj.name === "hp" ).value)
+      return -1;
+    return 0;
+  }
+
+  sortDefault(a,b) {
+    if (a.id < b.id)
+      return -1;
+    if (a.id > b.id)
+      return 1;
+    return 0;
+  }
+
+
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <SortForm handleSort={this.handleSort}/>
         <br />
-        <PokemonCollection />
+        <Search onSearchChange={ (e) => this.handleSearch(e) } showNoResults={false} />
         <br />
-        <PokemonForm />
+        <PokemonCollection pokemon={this.state.filteredPokemon}/>
+        <br />
+        <PokemonForm createPokemon={this.createPokemon}/>
       </div>
     )
   }
